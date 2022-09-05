49f620c5cd15399c595b01eab0074e5a8f48502a
hjhopp <hann.jo.hopp@gmail.com>
2022-09-04 19:26:57 -0700

    create board

--- snek.js
+++ snek.js
@@ -1,1 +1,46 @@
-console.log("Hello world");
+const $ = document;
+const body = $.getElementsByTagName("body")[0];
+
+function createLogo (parent = body) {
+    const logo = $.createElement("h1");
+
+    logo.innerText = "Snek";
+    logo.classList.add("logo");
+
+    parent.append(logo);
+}
+
+function createBoard (parent = body, gridSize = 1764) {
+    const board = $.createElement("div");
+    const rows = Math.sqrt(gridSize);
+    let row = 0;
+
+    board.classList.add("board");
+    board.style.left = `calc(var(--cell-width) * ${-(rows / 2)})`;
+
+    for (let i = 0; i < gridSize; i++) {
+        if (i % rows === 0 && i !== 0) {
+            row++;
+        }
+
+        const cell = $.createElement("div");
+
+        const x = i - row * rows;
+        const y = row;
+
+        cell.classList.add("cell");
+        cell.style.left = `calc(${x} * var(--cell-width))`;
+        cell.style.top = `calc(${y} * var(--cell-width))`;
+
+        cell.setAttribute("data-active", false);
+        cell.setAttribute("data-x", x);
+        cell.setAttribute("data-y", y);
+
+        board.append(cell);
+    }
+
+    parent.append(board);
+}
+
+createLogo();
+createBoard();
