4d6454a12c8f55b64d762ef2c4bf7e5e605f5686
hjhopp <hann.jo.hopp@gmail.com>
2022-09-04 17:56:15 -0700

    cleanup

--- test /setup.spec.js
+++ test /setup.spec.js
@@ -1,9 +1,11 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('homepage has Snek in title', async ({ page }) => {
-  await page.goto('');
+test.describe("Setting up Snek", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('');
+    });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Snek/);
+    test('homepage has Snek in title', async ({ page }) => {
+        await expect(page).toHaveTitle(/Snek/);
+    });
 });
