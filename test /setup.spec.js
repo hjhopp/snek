4d204a5fe90084d7b6cb0f63a533c1b491621217
hjhopp <hann.jo.hopp@gmail.com>
2022-09-04 20:25:08 -0700

    setup tests

--- test /setup.spec.js
+++ test /setup.spec.js
@@ -2,10 +2,22 @@ const { test, expect } = require('@playwright/test');
 
 test.describe("Setting up Snek", () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('');
+        await page.goto("");
     });
 
-    test('homepage has Snek in title', async ({ page }) => {
+    test("homepage has Snek in title", async ({ page }) => {
         await expect(page).toHaveTitle(/Snek/);
+    });
+
+    test("Game has a logo", async ({ page }) => {
+        const logo = await page.locator("h1");
+
+        await expect(logo).toHaveCount(1);
+    });
+
+    test("Game has a board", async ({ page }) => {
+        const board = await page.locator("div.board");
+
+        await expect(board).toHaveCount(1);
     });
 });
